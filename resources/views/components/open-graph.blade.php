8ed278517574502d1d2767b33cc6e847f30e67dd
Dean Blackborough <dean@g3d-development.com>
2022-09-28 16:39:25 +0100

    Social cards
    - Added OpenGraph and Twitter card meta tags

---
+++ resources/views/components/open-graph.blade.php
@@ -0,0 +1,10 @@
+    <meta property="og:url" content="https://budget.costs-to-expect.comub.com">
+    <meta property="og:site_name" content="Budget by Costs to Expect">
+    <meta property="og:title" content="A free, open source budgeting tool powered by the Costs to Expect API.">
+    <meta property="og:description" content="Budget is a free open source budgeting tool that is so easy to use a child could manage your budget.">
+    <meta property="og:image" content="https://budget.costs-to-expect.com/images/logo.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="190">
+    <meta property="og:image:height" content="190">
+    <meta property="og:image:alt" content="Budget by Costs to Expect">
+    <meta property="og:type" content="website">
---
+++ resources/views/components/twitter-card.blade.php
@@ -0,0 +1,7 @@
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@coststoexpect" />
+    <meta name="twitter:creator" content="@DBlackborough" />
+    <meta name="twitter:title" content="Budget by Costs to Expect" />
+    <meta name="twitter:description" content="Budget is a free open source budgeting tool that is so easy to use a child could manage your budget." />
+    <meta name="twitter:image" content="https://budget.costs-to-expect.com/images/logo.png" />
+    <meta name="twitter:image:alt" content="Budget by Costs to Expect" />
