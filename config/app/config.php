34f383a6778b8785a6f64ef33d7c5efdc9efcbd1
Dean Blackborough <dean@g3d-development.com>
2022-10-04 13:21:45 +0100

    Release
    - Set the version

--- config/app/config.php
+++ config/app/config.php
@@ -10,6 +10,6 @@ return [
     'item_subtype_id' => env('ITEM_SUBTYPE_ID'),
     'cookie_user' => env('SESSION_NAME_USER'),
     'cookie_bearer' => env('SESSION_NAME_BEARER'),
-    'version' => 'v0.02.0',
-    'release_date' => '5th September 2022'
+    'version' => 'v0.03.0',
+    'release_date' => '4th October 2022'
 ];
