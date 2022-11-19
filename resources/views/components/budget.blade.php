17ca973f36450b62c706b5f15d6ff84bcceb374c
Dean Blackborough <dean@g3d-development.com>
2022-11-19 17:10:40 +0000

    Smaller
    - Reduce the size

--- resources/views/components/budget.blade.php
+++ resources/views/components/budget.blade.php
@@ -50,7 +50,7 @@
                         <div class="col-12 expense @if ($active_item === $__item->id() && $__month->year() === $active_item_year && $__month->month() === $active_item_month) active shadow @endif" @if($__item->disabled() === true) title="Disabled expense" @endif>
                             <div class="name text-grey" title="{{ $__item->name() }}">
                                 <p class="mb-0">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-fill float-start" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-credit-card-fill float-start mt-1" viewBox="0 0 12 12">
                                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"/>
                                     </svg>
                                     {{ $__item->name() }}
