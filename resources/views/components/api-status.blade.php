b095e5b248bbad858c9e1d1ae3c578666747cf8d
Dean Blackborough <dean@g3d-development.com>
2023-01-31 23:55:32 +0000

    Fixes
    - Navbar will not longer change colour
    - Spelling error

--- resources/views/components/api-status.blade.php
+++ resources/views/components/api-status.blade.php
@@ -1,6 +1,6 @@
     @env(['staging', 'local'])
         @if (array_key_exists('environment', $api_status) && array_key_exists('cache', $api_status))
-            <div class="container-fluid d-flex justify-content-end">
+            <div class="container-fluid d-flex justify-content-end bg-dark">
                 <div class="d-flex align-items-center">
                     <span class="badge bg-success me-2">API Environment: {{ $api_status['environment'] }}</span>
                     <span class="badge bg-success">API Cache: @if($api_status['cache'] === true) Enabled @else Disabled @endif</span>
