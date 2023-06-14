18015d1d177a0510f535d03a530338b614dad257
Dean Blackborough <dean@g3d-development.com>
2023-06-14 15:22:28 +0100

    Release
    - Date correction
    - Added release date and version

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -97,7 +97,7 @@
                         the downgrade but no data will be lost.</p>
 
                     <h3 class="display-6 mt-3 mb-3">When will Budget Pro release?</h3>
-                    <p class="lead">Budget Pro was released into alpha on the 1st June. We are half through finishing
+                    <p class="lead">Budget Pro was released into alpha on the 2nd June. We are half through finishing
                         the development and pushing for a beta release in August/September.</p>
 
                     <h3 class="display-6 mt-3 mb-3">Will you update Budget after you release Budget Pro?</h3>
