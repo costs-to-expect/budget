8ff65a1c54f2fff16eb88db16a43e5bca74fabe3
Dean Blackborough <dean@g3d-development.com>
2025-06-10 21:48:29 +0100

    Sign in
    - Sign in updated to the new design

--- resources/views/authentication/sign-in.blade.php
+++ resources/views/authentication/sign-in.blade.php
@@ -31,50 +31,53 @@
 
                         @csrf
 
-                        <label for="email" class="block text-base font-medium text-gray-700">
-                            Email <span class="text-lg">*</span>
-                        </label>
-                        <div class="mt-1">
-                            <input id="email" name="email" type="email" required 
-                            class="block w-full rounded-md border border-gray-300 px-3 py-2
-            placeholder-gray-400 shadow-sm focus:border-pinky-500 focus:outline-none focus:ring-pinky-500 @if($errors !== null && array_key_exists('email', $errors)) border-red-500 ring-1 ring-red-500 @endif sm:text-sm"
-                                   placeholder="email@email.com" value="{{ old('email') }}" />
-                            <p class="mt-2 text-sm text-gray-500">Please enter your email address</p>
-                            @if($errors !== null && array_key_exists('email', $errors) && is_array($errors['email']))
-                                @if (count($errors['email']['errors']) === 1)
-                                    <p class="mt-2 text-sm text-red-500 pl-2">{{ $errors['email']['errors'][0] }}</p>
-                                @else
-                                    <ul class="mt-2 text-sm text-red-500 list-disc pl-6">
-                                        @foreach ($errors['email']['errors'] as $__error)
-                                            <li>{{ $__error }}</li>
-                                        @endforeach
-                                    </ul>
+                        <div>
+                            <label for="email" class="block text-base font-medium text-gray-700">
+                                Email <span class="text-lg">*</span>
+                            </label>
+                            <div class="mt-1">
+                                <input id="email" name="email" type="email" required 
+                                class="block w-full rounded-md border border-gray-300 px-3 py-2
+                placeholder-gray-400 shadow-sm focus:border-pinky-500 focus:outline-none focus:ring-pinky-500 @if($errors !== null && array_key_exists('email', $errors)) border-red-500 ring-1 ring-red-500 @endif sm:text-sm"
+                                       placeholder="email@email.com" value="{{ old('email') }}" />
+                                <p class="mt-2 text-sm text-gray-500">Please enter your email address</p>
+                                @if($errors !== null && array_key_exists('email', $errors) && is_array($errors['email']))
+                                    @if (count($errors['email']['errors']) === 1)
+                                        <p class="mt-2 text-sm text-red-500 pl-2">{{ $errors['email']['errors'][0] }}</p>
+                                    @else
+                                        <ul class="mt-2 text-sm text-red-500 list-disc pl-6">
+                                            @foreach ($errors['email']['errors'] as $__error)
+                                                <li>{{ $__error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
                                 @endif
-                            @endif
-                        </div>
-
-                        {{--<div>
-                            <x-helper.form.field.email
-                                    name="email"
-                                    title="Email address"
-                                    :required="true"
-                                    :hasError="$errors !== null && array_key_exists('email', $errors)"
-                                    :value="old('email')"
-                            />
-                            <p class="mt-2 text-sm text-gray-500">Please enter your email address</p>
-                            <x-helper.form.api-error :errors="$errors['email'] ?? null" />
+                            </div>
                         </div>
 
                         <div>
-                            <x-helper.form.field.password
-                                    name="password"
-                                    title="Your password"
-                                    :required="true"
-                                    :hasError="$errors !== null && array_key_exists('password', $errors)"
-                            />
-                            <p class="mt-2 text-sm text-gray-500">Please enter your password</p>
-                            <x-helper.form.api-error :errors="$errors['password'] ?? null" />
-                        </div>--}}
+                            <label for="password" class="block text-base font-medium text-gray-700">
+                                Password <span class="text-lg">*</span>
+                            </label>
+                            <div class="mt-1">
+                                <input id="password" name="password" type="password" required 
+                                class="block w-full sm:max-w-sm rounded-md border border-gray-300 px-3 py-2
+                placeholder-gray-400 shadow-sm focus:border-pinky-500 focus:outline-none focus:ring-pinky-500 @if($errors !== null && array_key_exists('password', $errors)) border-red-500 ring-1 ring-red-500 @endif sm:text-sm" />
+                                <p class="mt-2 text-sm text-gray-500">Please enter your password, <em>we will check this
+                                        against the hashed value in our database</em>.</p>
+                                @if($errors !== null && array_key_exists('password', $errors) && is_array($errors['password']))
+                                    @if (count($errors['password']['errors']) === 1)
+                                        <p class="mt-2 text-sm text-red-500 pl-2">{{ $errors['password']['errors'][0] }}</p>
+                                    @else
+                                        <ul class="mt-2 text-sm text-red-500 list-disc pl-6">
+                                            @foreach ($errors['password']['errors'] as $__error)
+                                                <li>{{ $__error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="flex items-center justify-between">
                             <div class="flex items-center">
@@ -89,27 +92,32 @@
         px-1.5 py-1 md:px-2 md:py-1.5 text-base font-medium text-white shadow-sm bg-pinky-500 hover:bg-pinky-700">
                                 Sign-in
                             </button>
+
+                            <a href="{{ route('landing') }}" class="rounded-md border border-transparent
+    bg-black px-1.5 py-1 md:px-2 md:py-1.5 text-base font-medium text-white shadow-sm hover:bg-gray-700">
+                                Cancel
+                            </a>
                         </div>
                     </form>
                 </div>
 
                 <div class="py-6 px-4 sm:px-10">
                     <p class="text-sm text-gray-500">
-                        By using Budget, you are agreeing to our 
-                        <a href="{{ route('privacy-policy') }}">privacy policy</a>. Please read our privacy policy 
-                        carefully before registering. In short, we <strong>do not</strong> track you and we 
-                        <strong>will not</strong> share your data.
+                        By using Budget, you are agreeing to our
+                        <x-helper.control.link.text :route="route('privacy-policy')" label="privacy policy." />
+                        Please read our privacy policy carefully before registering. In short, we 
+                        <strong>do not</strong> track you and we <strong>will not</strong> share your data.
                     </p>
                     
                     <p class="mt-2 text-sm text-gray-500">
-                        If you don't have an account with Costs to Expect, you can 
-                        <a href="{{ route('register.view') }}">register</a> to get access to Budget and the 
-                        entire Costs to Expect service.
+                        If you don't have an account with Costs to Expect, you can
+                        <x-helper.control.link.text :route="route('register.view')" label="register" />
+                        to get access to Budget and the entire Costs to Expect service.
                     </p>
 
                     <p class="mt-2 text-sm text-gray-500">
                         If you have forgotten your password, please use our
-                        <a href="{{ route('forgot-password.view') }}">forgot password</a>
+                        <x-helper.control.link.text :route="route('forgot-password.view')" label="forgot password" />
                         form and we will issue instructions on how to create a new password.
                     </p>
                 </div>
